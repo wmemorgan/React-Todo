b0451b03d34854162693cb232ac56f3242ab2817
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-03-06 02:03:01 +0800

    Revert ToDoForm back to function

--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -1,23 +1,16 @@
-import React , {Component} from 'react'
+import React from 'react'
 
-class ToDoForm extends Component {
-  constructor(props) {
-    super(props)
-    // this.state = { inputValue: ''}
-  }
-
-  render() {
-    const { value, onAddTask, onInputChange, onClearCompletedTasks } = this.props
-    return (
-      <div className="todo-form">
-        <form onSubmit={onAddTask}>
-          <input name="task" onChange={onInputChange} value={value} placeholder="Enter task"/>
-          <button type="submit" className="btn btn-add" >Add Todo</button>
-          <button className="btn btn-clear" onClick={onClearCompletedTasks}>Clear Completed</button>
-        </form>
-      </div>
-    )
-  }
+const ToDoForm = props => {
+  const { value, onAddTask, onInputChange, onClearCompletedTasks } = props
+  return (
+    <div className="todo-form">
+      <form onSubmit={onAddTask}>
+        <input name="task" onChange={onInputChange} value={value} placeholder="Enter task"/>
+        <button type="submit" className="btn btn-add" >Add Todo</button>
+        <button className="btn btn-clear" onClick={onClearCompletedTasks}>Clear Completed</button>
+      </form>
+    </div>
+  )
 }
 
 export default ToDoForm
