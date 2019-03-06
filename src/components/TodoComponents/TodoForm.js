2d954ec8bc002245020f2ae5936c85267d48951d
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-03-06 20:33:30 +0800

    Move App title
    Add form container

--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -3,13 +3,15 @@ import React from 'react'
 const ToDoForm = props => {
   const { inputValue, addTask, enterTask, clearCompletedTasks } = props
   return (
-    <div className="todo-form">
-      <form onSubmit={addTask}>
-        <input name="task" onChange={enterTask} value={inputValue} placeholder="Enter task"/>
-        <button type="submit" className="btn btn-add" >Add Todo</button>
+    <div className="todo-form-container">
+      <h2>To Do List</h2>
+      <form onSubmit={addTask} className="todo-form">
+        <input name="task" onChange={enterTask} value={inputValue} placeholder="Enter task" />
+        <button type="submit" className="btn btn-add" >Add Task</button>
         <button className="btn btn-clear" onClick={clearCompletedTasks}>Clear Completed</button>
       </form>
     </div>
+
   )
 }
 
--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -6,7 +6,6 @@ const ToDoList = (props) => {
   const { tasks, toggleComplete } = props
   return (
     <div className="todo-list">
-      <h2>To Do List</h2>
       <ol className="todo-list-container">
         {tasks.map(task => (
           <ToDo task={task} key={task.id} toggleComplete={toggleComplete} />
