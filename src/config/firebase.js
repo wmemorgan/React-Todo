b3c5ca848f314273988e8691931f128534c57f3d
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-03-08 16:36:02 +0800

    Add firebase module

---
+++ src/config/firebase.js
@@ -0,0 +1,9 @@
+const config = {
+  apiKey: process.env.APIKEY,
+  authDomain: "wme-react-todo.firebaseapp.com",
+  databaseURL: "https://wme-react-todo.firebaseio.com",
+  projectId: "wme-react-todo",
+  storageBucket: "wme-react-todo.appspot.com",
+}
+
+export default config
